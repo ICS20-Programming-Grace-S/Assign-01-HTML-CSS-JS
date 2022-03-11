4b96b22693e9b89a40561599afbd357b1a1fab83
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-03-11 16:07:18 +0000

    Edit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Grace s All rights reserved
+//
+// Created by: Grace S
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "My Text for Button!"
+  
+  document.getElementById('hello-world').innerHTML ='Hello there! Welcome the Asia! We are the largest and most populated conteinent in the WORLD. There are millions of places where you can come an travel here, as we have 48 different countries!'
+}
